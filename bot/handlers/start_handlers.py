5a0e7380cf6dec67d30940875a24adfcac8fa8bd
orriginalo <originalo1717@gmail.com>
2025-12-06 17:13:57 +0400

    fix: state clear in /start handler

--- bot/handlers/start_handlers.py
+++ bot/handlers/start_handlers.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
 
 from .utils import send_main_menu
 
@@ -8,5 +9,6 @@ router = Router()
 
 
 @router.message(CommandStart())
-async def start(msg: Message):
+async def start(msg: Message, state: FSMContext):
+    await state.clear()
     await send_main_menu(msg.chat.id, msg)
